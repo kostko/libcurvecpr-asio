02c35cfa0d429b7c78bd7ca67ee15f18d01907ae
Jernej Kos <jernej@kos.mx>
2014-06-13 19:17:18 +0200

    Use per-connection buffers.

--- libcurvecpr-asio/examples/simple_server.cpp
+++ libcurvecpr-asio/examples/simple_server.cpp
@@ -4,13 +4,32 @@
 #include <list>
 #include <thread>
 
+class connection {
+public:
+  connection(size_t id, boost::asio::io_service &service)
+    : id(id),
+      stream(service),
+      buffer_space(1024),
+      received_data(0)
+  {
+  }
+public:
+  /// Connection identifier
+  size_t id;
+  /// Stream
+  curvecp::stream stream;
+  /// Buffer space
+  std::vector<char> buffer_space;
+  /// Received bytes counter
+  std::size_t received_data;
+};
+
 class example {
 public:
   example(boost::asio::io_service &service)
     : service_(service),
       acceptor_(service),
-      buffer_space_(1024),
-      received_data_(0)
+      next_connection_id_(0)
   {
     acceptor_.set_local_extension(std::string("\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00", 16));
     acceptor_.set_local_public_key(std::string("\x3f\x56\xfd\x60\x4f\x31\x57\x5d\x1f\xa8\xd2\x4\x2e\x8a\xd7\xe1\x1e\x8a\x51\x64\xf0\x79\xb7\x63\x63\x14\xcd\x52\x9e\x7a\x9a\x19", 32));
@@ -25,63 +44,72 @@ public:
       10000
     ));
 
-    boost::shared_ptr<curvecp::stream> peer(boost::make_shared<curvecp::stream>(service_));
-    acceptor_.async_accept(*peer, boost::bind(&example::accept_handler, this, peer, _1));
+    boost::shared_ptr<connection> peer(boost::make_shared<connection>(next_connection_id_++, service_));
+    acceptor_.async_accept(
+      peer->stream,
+      boost::bind(&example::accept_handler, this, peer, _1)
+    );
     acceptor_.listen();
   }
 
-  void accept_handler(boost::shared_ptr<curvecp::stream> stream,
+  void accept_handler(boost::shared_ptr<connection> peer,
                       const boost::system::error_code &ec)
   {
     std::cout << "ACCEPTOR: Accept handler called, we have a new stream!" << std::endl;
-    boost::asio::async_read(*stream,
-      boost::asio::buffer(&buffer_space_[0], 64),
-      boost::bind(&example::read_handler, this, stream, _1, _2)
+    boost::asio::async_read(
+      peer->stream,
+      boost::asio::buffer(&peer->buffer_space[0], 64),
+      boost::bind(&example::read_handler, this, peer, _1, _2)
     );
 
     // Get ready for the next connection
-    boost::shared_ptr<curvecp::stream> peer(boost::make_shared<curvecp::stream>(service_));
-    acceptor_.async_accept(*peer, boost::bind(&example::accept_handler, this, peer, _1));
+    boost::shared_ptr<connection> next_peer(boost::make_shared<connection>(next_connection_id_++, service_));
+    acceptor_.async_accept(
+      next_peer->stream,
+      boost::bind(&example::accept_handler, this, next_peer, _1)
+    );
   }
 
-  void close_handler(boost::shared_ptr<curvecp::stream> stream)
+  void close_handler(boost::shared_ptr<connection> peer)
   {
-    std::cout << "STREAM: Stream closed." << std::endl;
+    std::cout << "STREAM[" << peer->id << "]: Stream closed." << std::endl;
   }
 
-  void read_handler(boost::shared_ptr<curvecp::stream> stream,
+  void read_handler(boost::shared_ptr<connection> peer,
                     const boost::system::error_code &ec,
                     std::size_t bytes)
   {
     if (ec) {
-      std::cout << "STREAM: Error ocurred while reading!" << std::endl;
-      stream->async_close(boost::bind(&example::close_handler, this, stream));
+      std::cout << "STREAM[" << peer->id << "]: Error ocurred while reading!" << std::endl;
+      peer->stream.async_close(boost::bind(&example::close_handler, this, peer));
       return;
     }
 
-    received_data_ += bytes;
-    if (received_data_ % 1024 == 0)
-      std::cout << "STREAM: Read " << received_data_ << " bytes." << std::endl;
+    peer->received_data += bytes;
+    if (peer->received_data % 1024 == 0)
+      std::cout << "STREAM[" << peer->id << "]: Read " << peer->received_data << " bytes." << std::endl;
 
-    boost::asio::async_write(*stream,
-      boost::asio::buffer(&buffer_space_[0], bytes),
-      boost::bind(&example::write_handler, this, stream, _1, _2)
+    boost::asio::async_write(
+      peer->stream,
+      boost::asio::buffer(&peer->buffer_space[0], bytes),
+      boost::bind(&example::write_handler, this, peer, _1, _2)
     );
   }
 
-  void write_handler(boost::shared_ptr<curvecp::stream> stream,
+  void write_handler(boost::shared_ptr<connection> peer,
                      const boost::system::error_code &ec,
                      std::size_t bytes)
   {
     if (ec) {
-      std::cout << "STREAM: Error ocurred while writing!" << std::endl;
-      stream->async_close(boost::bind(&example::close_handler, this, stream));
+      std::cout << "STREAM[" << peer->id << "]: Error ocurred while writing!" << std::endl;
+      peer->stream.async_close(boost::bind(&example::close_handler, this, peer));
       return;
     }
 
-    boost::asio::async_read(*stream,
-      boost::asio::buffer(&buffer_space_[0], 64),
-      boost::bind(&example::read_handler, this, stream, _1, _2)
+    boost::asio::async_read(
+      peer->stream,
+      boost::asio::buffer(&peer->buffer_space[0], 64),
+      boost::bind(&example::read_handler, this, peer, _1, _2)
     );
   }
 private:
@@ -91,10 +119,8 @@ private:
   boost::asio::io_service &service_;
   /// CurveCP acceptor
   curvecp::acceptor acceptor_;
-  /// Buffer space
-  std::vector<char> buffer_space_;
-  /// Received bytes counter
-  std::size_t received_data_;
+  /// Connection counter
+  size_t next_connection_id_;
 };
 
 int main()
